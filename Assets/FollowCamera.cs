03d365ee425052c7c2f4d298ba80f62c3f414685
ph4 <22990490+ph4@users.noreply.github.com>
2020-06-29 00:42:36 +0300

    init

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] GameObject follow;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float rotationSpeed = 5f;
+    [SerializeField] float offsetX = 2f;
+    [SerializeField] float offsetY = 1f;
+
+    private Vector3 _velocity;
+    private Quaternion _lookRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var followTransform = follow.transform;
+
+        var posFollow = follow.transform.position;
+        var pos = posFollow;
+        pos.y += offsetY;
+        pos.x += offsetX;
+        transform.position = pos;
+
+        var asin = Mathf.Acos(offsetY / offsetX);
+        var rotZ = asin * Mathf.Rad2Deg;
+        _lookRotation = Quaternion.AngleAxis(-rotZ, Vector3.right);
+        transform.Rotate(Vector3.right, rotZ, Space.Self);
+    }
+
+    private void FixedUpdate()
+    {
+        //transform.SetPositionAndRotation(pos, transform.rotation);
+      
+        transform.Rotate(transform.right, rotationSpeed * Time.deltaTime, Space.World);
+        
+        _velocity = Vector3.forward * (speed * Time.deltaTime);
+        _velocity = _lookRotation * _velocity;
+        
+        transform.Translate(_velocity, Space.Self);
+    }
+}
---
+++ Assets/Orbiter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbiter : MonoBehaviour
+{
+    [SerializeField] GameObject mainSphere;
+    [SerializeField, Range(0f, 10f)] float height = 1f;
+    [SerializeField, Range(0f, 20f)] float angularSpeed = 1f;
+
+    private float _mainRadius;
+    private float _radius;
+    private Vector3 _position;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _mainRadius = mainSphere.transform.lossyScale.x / 2;
+        _radius = _mainRadius + height;
+        
+        var pos = mainSphere.transform.position;
+        pos.y += _radius;
+        transform.position = pos;
+    }
+
+    private void FixedUpdate()
+    {
+        _radius = _mainRadius + height;
+        transform.RotateAround(
+            mainSphere.transform.position,
+            mainSphere.transform.forward,
+            angularSpeed * Time.deltaTime);
+    }
+}
